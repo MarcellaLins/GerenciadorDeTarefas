fdcd81fb3a58a96bd6c32c45f52dd9ba82626812
Marcella Viana <vianamarcella05@gmail.com>
2026-02-27 14:54:15 -0300

    interface creation

---
+++ src/IfQueue.java
@@ -0,0 +1,8 @@
+public interface IfQueue {
+    void enqueue(int data);
+    int dequeue();
+    int size();
+    int head();
+    boolean isEmpty();
+
+}
---
+++ src/IfStack.java
@@ -0,0 +1,7 @@
+public interface IfStack {
+    void push(int value);
+    int pop();
+    int peek();
+    boolean isEmpty();
+
+}
