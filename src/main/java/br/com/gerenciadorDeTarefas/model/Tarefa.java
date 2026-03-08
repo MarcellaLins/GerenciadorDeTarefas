3306cdf8fe4f32c0a9fc6c39b6e4a0dee16b3e00
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2026-03-08 15:06:33 -0300

    feat: classe base de Tarefas (construtor, getters e setters)

--- src/main/java/br/com/gerenciadorDeTarefas/model/Tarefa.java
+++ src/main/java/br/com/gerenciadorDeTarefas/model/Tarefa.java
@@ -4,11 +4,49 @@ import java.time.LocalDate;
 
 public class Tarefa {
 
-    private String name;
-    private String descricao;
-    private Categoria categoria;
-    private LocalDate prazo;
+    private String title;
+    private String description;
+    private Categoria category;
+    private LocalDate deadline;
 
+    public Tarefa(Categoria category, LocalDate deadline, String description, String title) {
+        this.category = category;
+        this.deadline = deadline;
+        this.description = description;
+        this.title = title;
+    }
 
+    //GETTERS E SETTERS
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public Categoria getCategory() {
+        return category;
+    }
+
+    public void setCategory(Categoria category) {
+        this.category = category;
+    }
 }
